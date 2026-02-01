0d5a0997d3a5aba8968dd4b9625067c6fb98480e
June Pyle <github@ruleofsix.ca>
2026-01-31 19:18:27 -0800

    Fix broken project configuration and finish IDevice and IPod classes.

--- src/code/IDevice.java
+++ src/code/IDevice.java
@@ -8,7 +8,7 @@ public abstract class IDevice
 {
     private final String purpose;
 
-    public IDevice(final String purpose)
+    protected IDevice(final String purpose)
     {
         validatePurpose(purpose);
 
@@ -28,6 +28,52 @@ public abstract class IDevice
     public final String getPurpose()
     {
         return purpose;
+    }
+
+    /**
+     * Creates a String representation of the IDevice.
+     *
+     * @return a String containing the IDevice's purpose
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("Purpose: %s\n",
+                             purpose);
+    }
+
+    /**
+     * Determines whether this IDevice is equal to a given object. IDevices can only be equal to other non-null
+     * IDevices. IDevices are considered equal if and only if their purposes are the same.
+     *
+     * @param o   the reference object with which to compare
+     * @return true if o is an IDevice with an equal purpose to this one, false otherwise
+     */
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (o == null)
+        {
+            return false;
+        }
+
+        if (!(o instanceof final IDevice other)) {
+            return false;
+        }
+
+        return this.purpose.equals(other.getPurpose());
+    }
+
+    /**
+     * Generates a hash code value for the IDevice, determined by its purpose. Since equality of IDevices is
+     * based on their purpose, this upholds the hashCode() contract.
+     *
+     * @return a hash code value for the IDevice
+     */
+    @Override
+    public int hashCode()
+    {
+        return this.purpose.hashCode();
     }
 
     // Validates a purpose, which must not be null.
--- src/code/IPod.java
+++ src/code/IPod.java
@@ -19,7 +19,8 @@ public class IPod extends IDevice
      * @param songsStored the discrete number of songs currently stored on the IPod
      * @param maxVolumeDb the maximum volume of the IPod, in decibels
      */
-    public IPod(final int songsStored, final double maxVolumeDb)
+    public IPod(final int songsStored,
+                final double maxVolumeDb)
     {
         super(PURPOSE);
 
@@ -31,20 +32,115 @@ public class IPod extends IDevice
     }
 
     /**
-     * Prints the following details about this IPod to the console:
-     *     - Purpose
-     *     - Number of songs stored
-     *     - Maximum volume in decibels
+     * Prints the IPod's details to the console (see toString for specifics).
      */
     @Override
-    public void printDetails() {
-        System.out.println("Purpose: " + this.getPurpose());
-        System.out.println("Number of songs stored: " + this.songsStored);
-        System.out.println("Maximum volume (decibels): " + this.maxVolumeDb);
+    public void printDetails()
+    {
+        System.out.println(this);
     }
 
-    public int getSongsStored() {
+    /**
+     * Generates a String representation of the IPod, containing all of its fields.
+     *
+     * @return A String with the IPod's:
+     * - Purpose
+     * - Number of songs stored
+     * - Maximum number in decibels
+     */
+    @Override
+    public String toString()
+    {
+        final StringBuilder toStringBuilder;
+        final String superString;
+
+        superString = super.toString();
+        toStringBuilder = new StringBuilder(superString);
+
+        toStringBuilder.append("Songs stored: ");
+        toStringBuilder.append(songsStored);
+        toStringBuilder.append('\n');
+        toStringBuilder.append("Maximum volume (decibels): ");
+        toStringBuilder.append(maxVolumeDb);
+        toStringBuilder.append('\n');
+
+        return toStringBuilder.toString();
+    }
+
+    /**
+     * Determines whether this IPod is equal to a given object. IPods can only be equal to other non-null
+     * IPods. IPods are considered equal if and only if they have the same number of songs stored on them.
+     *
+     * @param o   the reference object with which to compare
+     * @return true if o is an IPod with the same amount of songs stored on it as this one, false otherwise
+     */
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (o == null)
+        {
+            return false;
+        }
+
+        if (!(o instanceof final IPod other)) {
+            return false;
+        }
+
+        return this.songsStored == other.getSongsStored();
+    }
+
+    /**
+     * Generates a hash code value for the IPod, determined by the number of songs stored on it. Since equality of
+     * IPods is based on the number of songs stored on them, this upholds the hashCode() contract.
+     *
+     * @return a hash code value for the IPod
+     */
+    @Override
+    public int hashCode()
+    {
         return this.songsStored;
+    }
+
+    /**
+     * Accessor for songsStored.
+     *
+     * @return the number of songs currently stored on the IPod.
+     */
+    public final int getSongsStored()
+    {
+        return this.songsStored;
+    }
+
+    /**
+     * Mutator for songsStored.
+     *
+     * @param songsStored the new number of songs stored on the IPod.
+     */
+    public final void setSongsStored(final int songsStored)
+    {
+        validateSongsStored(songsStored);
+        this.songsStored = songsStored;
+    }
+
+    /**
+     * Accessor for maxVolumeDb.
+     *
+     * @return the current maximum volume of the IPod, in decibels.
+     */
+    public final double getMaxVolumeDb()
+    {
+        return this.maxVolumeDb;
+    }
+
+    /**
+     * Mutator for maxVolumeDb.
+     *
+     * @param maxVolumeDb the new maximum volume of the IPod, in decibels.
+     */
+    public final void setMaxVolumeDb(final double maxVolumeDb)
+    {
+        validateMaxVolumeDb(maxVolumeDb);
+        this.maxVolumeDb = maxVolumeDb;
     }
 
     // Validates the number of songs stored on the IPod, which cannot be less than MIN_SONGS_STORED
